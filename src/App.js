67fc2896fe92c78f4ddd277e5137a514fee9988e
Godfather <parag.parag110@gmail.com>
2025-04-29 06:49:48 +0600

    Fixed App.js structure clean

--- src/App.js
+++ src/App.js
@@ -6,33 +6,7 @@ import ThankYou from './pages/ThankYou';
 
 function App() {
   return (
-    <Router>import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
-    import Home from './pages/Home';
-    import SearchDonor from './pages/SearchDonor';
-    import RequestBlood from './pages/RequestBlood';
-    import ThankYou from './pages/ThankYou';
-    
-    function App() {
-      return (
-        <Router>
-          <nav style={{ marginBottom: "20px" }}>
-            <Link to="/" style={{ marginRight: "10px" }}>Home</Link>
-            <Link to="/search" style={{ marginRight: "10px" }}>Search Donor</Link>
-            <Link to="/request" style={{ marginRight: "10px" }}>Request Blood</Link>
-          </nav>
-    
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/search" element={<SearchDonor />} />
-            <Route path="/request" element={<RequestBlood />} />
-            <Route path="/thankyou" element={<ThankYou />} />
-          </Routes>
-        </Router>
-      );
-    }
-    
-    export default App;
-    
+    <Router>
       <nav style={{ marginBottom: "20px" }}>
         <Link to="/" style={{ marginRight: "10px" }}>Home</Link>
         <Link to="/search" style={{ marginRight: "10px" }}>Search Donor</Link>
